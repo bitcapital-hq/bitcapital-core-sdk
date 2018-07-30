ee226839c5a979f57dd21c8f00fa877125541442
Vitor Rangel <rangel.final@gmail.com>
2018-07-30 15:09:50 -0300

    Fix: Export all webservices

--- dist/types/services/index.d.ts
+++ dist/types/services/index.d.ts
@@ -1,6 +1,9 @@
-export { default as BaseModelWebService } from "./base/BaseModelWebService";
-export { default as UserWebService } from "./UserWebService";
 export { default as AnalyticsWebService } from "./AnalyticsWebService";
-export { default as OAuthWebService, OAuthWebServiceOptions } from "./OAuthWebService";
-export { default as OAuthClientWebService } from "./OAuthClientWebService";
+export { default as BaseModelWebService } from "./base/BaseModelWebService";
+export { default as ConsumerWebService } from "./ConsumerWebService";
+export { default as DomainWebService } from "./DomainWebService";
+export { default as MediatorWebService } from "./MediatorWebService";
 export { default as OAuthAccessTokenWebService } from "./OAuthAccessTokenWebService";
+export { default as OAuthClientWebService } from "./OAuthClientWebService";
+export { default as OAuthWebService, OAuthWebServiceOptions } from "./OAuthWebService";
+export { default as UserWebService } from "./UserWebService";
