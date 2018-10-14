d00e12ca0d43c60fab7bdbd7d4457b27c5383c52
Luis Brito <luiseduardo14@gmail.com>
2018-10-14 16:46:22 -0300

    Build: v1.1.2

--- dist/types/models/OAuth/index.d.ts
+++ dist/types/models/OAuth/index.d.ts
@@ -1,4 +1,4 @@
 export { default as OAuthCredentials, OAuthCredentialsSchema } from "./OAuthCredentials";
 export { default as OAuthClient, OAuthClientSchema, OAuthClientStatus } from "./OAuthClient";
 export { default as OAuthAccessToken, OAuthAccessTokenSchema, UserAgentInformation } from "./OAuthAccessToken";
-export { default as Scopes } from './scopes';
+export { default as Scopes } from "./scopes";
