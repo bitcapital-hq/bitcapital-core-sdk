66f9893c5195d2b09fee6036c5cd389b86ccf795
Vitor Rangel <rangel.final@gmail.com>
2019-01-16 11:34:56 -0200

    Feat: Update Wallet model

--- lib/models/Wallet/Wallet.ts
+++ lib/models/Wallet/Wallet.ts
@@ -1,33 +1,64 @@
-import User from "../User/User";
-import { WalletType } from "./WalletType";
+import User, { UserSchema } from "../User/User";
 import { BaseModel, BaseModelSchema } from "..";
-import { StellarWalletData, BankingWalletData } from "./WalletData";
+import { StellarWalletData, CardsWalletData } from "./WalletData";
 import { WalletBalance } from "./WalletBalance";
 import { IsNotEmpty, IsEnum } from "class-validator";
+import Asset, { AssetSchema } from "../Asset/Asset";
+import { TransactionSchema, Transaction } from "../Transaction";
+import Payment, { PaymentSchema } from "../Payment/Payment";
 
-export { WalletType, StellarWalletData, BankingWalletData, WalletBalance };
-
-export interface WalletSchema extends BaseModelSchema {
-  type: WalletType;
-  data?: StellarWalletData | BankingWalletData;
-  user?: User | string;
-  balances?: WalletBalance[];
+export enum WalletStatus {
+  PENDING = "pending",
+  REGISTERED = "registered",
+  READY = "ready",
+  FAILED = "failed"
 }
 
-export default class Wallet extends BaseModel implements WalletSchema {
+export enum WalletProvider {
+  CDT_CARDS = "cards",
+  NONE = "none"
+}
+
+export interface WalletSchema extends BaseModelSchema {
+  status: WalletStatus;
+  provider: WalletProvider;
+  stellar: StellarWalletData;
+  additionalData?: CardsWalletData;
+  balances?: WalletBalance[];
+  user: UserSchema;
+  issuedAssets: AssetSchema[];
+  assets: AssetSchema[];
+  transactions: TransactionSchema[];
+  received: PaymentSchema[];
+}
+
+export class Wallet extends BaseModel implements WalletSchema {
   @IsNotEmpty()
-  @IsEnum(WalletType)
-  type: WalletType = undefined;
+  @IsEnum(WalletStatus)
+  status: WalletStatus;
 
-  @IsNotEmpty() data: StellarWalletData | BankingWalletData = undefined;
+  @IsNotEmpty()
+  @IsEnum(WalletProvider)
+  provider: WalletProvider;
 
-  user?: User | string = undefined;
-  balances?: WalletBalance[] = undefined;
+  stellar: StellarWalletData;
+  additionalData?: CardsWalletData;
+  balances?: WalletBalance[];
+  user: User;
+  issuedAssets: Asset[];
+  assets: Asset[];
+  transactions: Transaction[];
+  received: Payment[];
 
   constructor(data: Partial<WalletSchema>) {
     super(data);
 
-    // Assign all props
-    Object.getOwnPropertyNames(this).map(prop => (this[prop] = data[prop]));
+    Object.assign(this, data);
+
+    this.user = data.user && new User(data.user);
+    this.issuedAssets = data.issuedAssets && data.issuedAssets.map(issuedAssets => new Asset(issuedAssets));
+    this.assets = data.assets && data.assets.map(asset => new Asset(asset));
+    this.transactions = data.transactions && data.transactions.map(transaction => new Transaction(transaction));
+    this.received = data.received && data.received.map(received => new Payment(received));
   }
 }
