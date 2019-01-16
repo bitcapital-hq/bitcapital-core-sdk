c45707eac89df7e20062843f5ed4e06775c409cf
Vitor Rangel <rangel.final@gmail.com>
2019-01-16 10:33:07 -0200

    Feat: Update payment model

--- lib/models/Payment/Payment.ts
+++ lib/models/Payment/Payment.ts
@@ -1,25 +1,36 @@
 import { BaseModel, BaseModelSchema } from "..";
-import Wallet from "../Wallet/Wallet";
+import Wallet, { WalletSchema } from "../Wallet/Wallet";
 import Transaction from "../Transaction/Transaction";
-import Recipient from "./Recipient";
+import Asset, { AssetSchema } from "../Asset/Asset";
+
+export enum PaymentType {
+  DEPOSIT = "deposit",
+  WITHDRAWAL = "withdrawal",
+  TRANSFER = "transfer"
+}
 
 export interface PaymentSchema extends BaseModelSchema {
-  source: Wallet | string;
-  recipients: Recipient[];
-  totalAmount: number;
-  transaction?: Transaction;
+  type: PaymentType;
+  transaction: Transaction;
+  amount: string;
+  destination: WalletSchema;
+  asset?: AssetSchema;
 }
 
 export default class Payment extends BaseModel implements PaymentSchema {
-  source: Wallet | string = undefined;
-  transaction: Transaction = undefined;
-  recipients: Recipient[] = undefined;
-  totalAmount: number = 0;
+  type: PaymentType;
+  transaction: Transaction;
+  amount: string;
+  destination: Wallet;
+  asset?: Asset;
 
   constructor(data: Partial<PaymentSchema>) {
     super(data);
 
-    // Assign all props
-    Object.getOwnPropertyNames(this).map(prop => (this[prop] = data[prop]));
+    Object.assign(this, data);
+
+    this.transaction = data.transaction && new Transaction(data.transaction);
+    this.destination = data.destination && new Wallet(data.destination);
+    this.asset = data.asset && new Asset(data.asset);
   }
 }
