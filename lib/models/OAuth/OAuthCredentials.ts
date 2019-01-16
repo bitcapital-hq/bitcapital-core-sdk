f3f2ef922fbf1652efc415b766dd56b3f62771ae
Vitor Rangel <rangel.final@gmail.com>
2019-01-16 10:56:52 -0200

    Fix: OAuthCredentials should implement it's schema

--- lib/models/OAuth/OAuthCredentials.ts
+++ lib/models/OAuth/OAuthCredentials.ts
@@ -10,22 +10,18 @@ export interface OAuthCredentialsSchema extends BaseModelSchema {
   scope: string[];
 }
 
-export default class OAuthCredentials extends BaseModel {
-  virtual?: boolean = false;
-  accessToken: string;
-  refreshToken?: string;
-  tokenType?: string;
-  userId?: string;
-  expiresAt?: Date;
+export default class OAuthCredentials extends BaseModel implements OAuthCredentialsSchema {
+  access_token: string;
+  refresh_token?: string;
+  token_type?: string;
+  expires_in?: number;
+  user_id?: string;
+  virtual?: boolean;
   scope: string[];
 
   constructor(data: Partial<OAuthCredentialsSchema>) {
     super(data);
-    this.accessToken = data.access_token;
-    this.refreshToken = data.refresh_token;
-    this.tokenType = data.token_type;
-    this.expiresAt = new Date(Date.now() + data.expires_in * 1000);
-    this.userId = data.user_id;
-    this.virtual = data.virtual || this.virtual;
+
+    Object.assign(this, data);
   }
 }
