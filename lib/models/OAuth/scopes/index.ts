fd30e382aa88b9e49f65ca0161bf1f1d6672c841
Vitor Rangel <rangel.final@gmail.com>
2019-01-16 10:26:48 -0200

    Feat: Update scopes

--- lib/models/OAuth/scopes/index.ts
+++ lib/models/OAuth/scopes/index.ts
@@ -1,17 +1,27 @@
 import { AssetsScopes } from "./assets";
 import { DomainsScopes } from "./domains";
+import { OAuthClientScopes } from "./oauth-clients";
 import { UsersScopes } from "./users";
 import { PaymentsScopes } from "./payments";
 import { TransactionsScopes } from "./transactions";
 import { WalletsScopes } from "./wallets";
 
-const scopes = {
+export type ScopeType =
+  | AssetsScopes
+  | DomainsScopes
+  | UsersScopes
+  | PaymentsScopes
+  | TransactionsScopes
+  | WalletsScopes;
+
+const Scopes = {
   assets: AssetsScopes,
   domains: DomainsScopes,
+  oAuthClients: OAuthClientScopes,
   users: UsersScopes,
   payments: PaymentsScopes,
   transactions: TransactionsScopes,
   wallets: WalletsScopes
 };
 
-export default scopes;
+export default Scopes;
---
+++ lib/models/OAuth/scopes/oauth-clients.ts
@@ -0,0 +1,8 @@
+export enum OAuthClientScopes {
+  /* Read scopes */
+  READ = "oauth-clients:read",
+  /* Write scopes */
+  WRITE = "oauth-clients:write",
+  /* Delete scopes */
+  DELETE = "oauth-clients:delete"
+}
