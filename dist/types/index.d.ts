6bac9e43999762b94e9a6492ed73e61f31da7aa6
Luis Brito <luiseduardo14@gmail.com>
2019-01-19 17:21:00 -0200

    Build: Distribution files

--- dist/types/index.d.ts
+++ dist/types/index.d.ts
@@ -1,4 +1,5 @@
 import Bitcapital from "./Bitcapital";
+export * from "bitcapital-common";
 export * from "./services";
 export * from "./utils";
 export * from "./session";
--- lib/index.ts
+++ lib/index.ts
@@ -1,5 +1,8 @@
 import Bitcapital from "./Bitcapital";
 
+/* Export common modules */
+export * from "bitcapital-common";
+
 /* Export all available modules */
 export * from "./services";
 export * from "./utils";
