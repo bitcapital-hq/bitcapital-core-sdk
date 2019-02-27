f9106977ed36ddd67c1ff4df26810b45a36cd60c
Vitor Rangel <rangel.final@gmail.com>
2019-02-27 17:42:26 -0300

    Fix: Remove obsolete methods from Consumer Web Service

--- lib/services/ConsumerWebService.ts
+++ lib/services/ConsumerWebService.ts
@@ -63,59 +63,6 @@ export class ConsumerWebService extends BaseModelWebService<User, UserSchema> {
   }
 
   /**
-   * Find the Documents from a User with role Consumer.
-   * This method won't return pictures.
-   *
-   * @param id The User ID.
-   * @deprecated This method was moved to DocumentWebService and will be removed in a future release
-   */
-  public async findDocumentsById(id: string = "me"): Promise<Document[]> {
-    console.warn("This method was moved to DocumentWebService and will be removed in a future release");
-
-    const response = await this.http.get(`/consumers/${id}/documents`);
-
-    if (!response || response.status !== 200) {
-      throw response;
-    }
-
-    return response.data.map(document => new Document(document));
-  }
-
-  /**
-   * Find the Documents from a User with role Consumer.
-   * This method will return pictures.
-   *
-   * @param id The User ID.
-   * @deprecated This method was moved to DocumentWebService and will be removed in a future release
-   */
-  public async findDocumentByIdAndType(id: string, type: DocumentType): Promise<Document> {
-    console.warn("This method was moved to DocumentWebService and will be removed in a future release");
-
-    const response = await this.http.get(`/consumers/${id}/documents/${type}`);
-
-    if (!response || response.status !== 200) {
-      throw response;
-    }
-
-    return new Document(response.data);
-  }
-
-  /**
-   * Find the Wallets from a User with role Consumer.
-   *
-   * @param id The User ID.
-   */
-  public async findWalletsById(id: string): Promise<Wallet[]> {
-    const response = await this.http.get(`/consumers/${id}/wallets`);
-
-    if (!response || response.status !== 200) {
-      throw response;
-    }
-
-    return response.data.map(wallet => new Wallet(wallet));
-  }
-
-  /**
    * Create a new User with role Consumer.
    *
    * @param consumer The User schema.
@@ -128,24 +75,6 @@ export class ConsumerWebService extends BaseModelWebService<User, UserSchema> {
     }
 
     return new User(response.data);
-  }
-
-  /**
-   * Create a new Document on a User with role Consumer.
-   *
-   * @param id The User ID.
-   * @deprecated This method was moved to DocumentWebService and will be removed in a future release
-   */
-  public async createDocument(id: string, document: DocumentSchema): Promise<Document> {
-    console.warn("This method was moved to DocumentWebService and will be removed in a future release");
-
-    const response = await this.http.post(`/consumers/${id}/documents`, document);
-
-    if (!response || response.status !== 200) {
-      throw response;
-    }
-
-    return new Document(response.data);
   }
 
   /**
@@ -174,60 +103,6 @@ export class ConsumerWebService extends BaseModelWebService<User, UserSchema> {
     }
 
     return new User(response.data);
-  }
-
-  /**
-   * Upload a new Document picture to a User with role Consumer.
-   *
-   * @param {string} id The User id.
-   * @param {DocumentType} type The Document type.
-   * @param {("front" | "back" | "selfie")} side The Document picture side.
-   * @param {File} picture The picture to be uploaded.
-   * @deprecated This method was moved to DocumentWebService and will be removed in a future release
-   */
-  public async uploadDocumentPicture(id: string, type: DocumentType, side: "front" | "back" | "selfie", picture: File) {
-    console.warn("This method was moved to DocumentWebService and will be removed in a future release");
-
-    const formData = new FormData();
-    formData.append("picture", picture);
-
-    const response = await this.http.post(`/consumers/${id}/documents/${type}/${side}`, formData, {
-      headers: {
-        "Content-Type": "multipart/form-data"
-      }
-    });
-
-    if (!response || response.status !== 200) {
-      throw response;
-    }
-
-    return new Document(response.data);
-  }
-
-  /**
-   * Upload a new Document picture to a User with role Consumer using base64.
-   *
-   * @param {string} id The User id.
-   * @param {DocumentType} type The Document type.
-   * @param {("front" | "back" | "selfie")} side The Document picture side.
-   * @param {string} picture The base64 representation of the picture to be uploaded.
-   * @deprecated This method was moved to DocumentWebService and will be removed in a future release
-   */
-  public async uploadDocumentPictureFromBase64(
-    id: string,
-    type: DocumentType,
-    side: "front" | "back" | "selfie",
-    picture: string
-  ) {
-    console.warn("This method was moved to DocumentWebService and will be removed in a future release");
-
-    const response = await this.http.post(`/consumers/${id}/documents/${type}/${side}`, { picture });
-
-    if (!response || response.status !== 200) {
-      throw response;
-    }
-
-    return new Document(response.data);
   }
 
   /**
